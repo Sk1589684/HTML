a7c849a15e783446a6d23694cf9689972c8b80a6
Sk1589684 <sarveshkulkarni.2003@gmail.com>
2026-03-30 11:13:32 +0530

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,19 @@
+function validate()
+{
+    var data = document.getElementById("a1").value;
+    var i = data.indexOf('@');
+    if(i>0)
+    {
+        var j = data.lastIndexOf('.');
+        if(j>i)
+        {
+            console.log("Valid Format");
+        }
+        else
+            console.log("Invalid Format");
+    }
+    else
+    {
+        console.log("Invalid Format");
+    }
+}
